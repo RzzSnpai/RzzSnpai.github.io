e5c35e9b8b3ed08c7257441b166e82e6e33b20ae
Eduardo <Rusoyoutube@hotmail.com>
2019-01-18 17:24:27 -0300

    commit

---
+++ precache-manifest.4cd9a11af602d2d5725fca38d000cda9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "018c95aec7015a220647",
+    "url": "/static/js/main.018c95ae.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "018c95aec7015a220647",
+    "url": "/static/css/main.78602a5b.chunk.css"
+  },
+  {
+    "revision": "87a2e1b34c7f05d93aec860491e34426",
+    "url": "/index.html"
+  }
+];
